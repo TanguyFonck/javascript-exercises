66fc0ee9ce01770dfefb7ef02077386906ddd491
TanguyFonck <121270006+TanguyFonck@users.noreply.github.com>
2023-02-02 12:27:47 +0100

    Finished exercises 1-7
    done with exercises 1-7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,33 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+   
+    if ( a < 0 || b < 0 ) {
+        return `ERROR`
+    }
+    else if (typeof a !== "number" || typeof b !== "number") {
+        return `ERROR`
+    }
+    else {
+        if ( a < b) {
+            let sum = 0
+            for ( i = a ; i <= b; i++)
+            sum += i
+            return sum
 
-};
+        }
 
+        if (b < a) {
+            let sum = 0
+            for ( i = b ; i <= a; i++)
+            sum += i
+            return sum
+        }
+
+
+    }
+    }
+
+
+
+// maak array van [begin, i, i2,i3...end]
 // Do not edit below this line
 module.exports = sumAll;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  let celcius = 0
+  celcius = (num - 32)* 0.556 
+  return Math.round(celcius* 10) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  let fahrenheit = 0
+  fahrenheit = (num * 1.8) + 32
+  return Math.round(fahrenheit * 10) / 10
 };
 
 // Do not edit below this line
